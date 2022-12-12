fc15256db9a23e929f99f8a03d2dad234840f93b
dv1veidis <limessi61@gmail.com>
2022-12-12 09:34:16 +0200

    Admin index fix
    Fixed crash on admin index

--- BlogSite/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ BlogSite/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -14,6 +14,7 @@ using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using BlogSite.Utility;
 
 namespace BlogSite.Areas.Identity.Pages.Account
 {
